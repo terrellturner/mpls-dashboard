8cf36960b0eb742952cc9cdeb49f8efd6f048938
terrellturner <terrell@rellsoft.dev>
2025-06-30 21:59:33 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <main className="flex justify-center p-1 lowercase items-center bg-stone-900 grow h-full text-stone-300">
+      <article className="p-6 rounded-lg border border-stone-500">
+        mpls dashboard
+      </article>
+    </main>
+  );
+}
+
+export default App;
