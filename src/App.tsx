8a4d983a1963849621e0713fee8baf2d550a30c8
terrellturner <terrell@rellsoft.dev>
2025-07-17 18:56:03 -0500

    Using Shadcn, strategy pattern + hooks setup for stat cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,44 @@
-import { useState } from "react";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import "./App.css";
 
 function App() {
+  const statConfigs = [
+    {
+      id: "crime-total",
+      endpoint: "/",
+      label: "",
+      hook: "",
+    },
+  ];
+
   return (
-    <main className="flex justify-center p-1 lowercase items-center bg-stone-900 grow h-full text-stone-300">
-      <article className="p-6 rounded-lg border border-stone-500">
-        mpls dashboard
+    <main className="flex justify-center p-10 lowercase items-center bg-stone-900 grow h-full text-stone-300">
+      <article className="p-6 rounded-lg border border-stone-500 w-full">
+        <Card className="aspect-square justify-around">
+          <CardHeader>
+            <CardTitle>Hello Minneapolis.</CardTitle>
+            <CardDescription className="fit-content">
+              It's {new Date().toLocaleDateString("en-US", { weekday: "long" })}
+              .
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <a
+              href="#"
+              className=" font-black inline-block text-3xl underline-offset-4 hover:underline"
+            >
+              {new Date().toLocaleDateString("en-US", {})}
+            </a>
+          </CardContent>
+        </Card>
       </article>
     </main>
   );
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,25 @@
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+// https://vite.dev/config/`
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_PROXY,
+          changeOrigin: true,
+        },
+      },
+    },
+  };
 });
